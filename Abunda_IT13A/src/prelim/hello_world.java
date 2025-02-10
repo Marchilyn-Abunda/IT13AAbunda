469fd7dd2182ca24bb99f864019058c01125c1ed
marchilyn <marchilynabunda@gmail.com>
2025-02-10 10:02:17 +0800

    Update hello_world.java

--- Abunda_IT13A/src/prelim/hello_world.java
+++ Abunda_IT13A/src/prelim/hello_world.java
@@ -6,7 +6,7 @@ public class hello_world {
     
     public static void main(String [] args){
         
-        System.out.println("Hello World! \n \tI'm Marchilyn Abunda");
+        System.out.println("Hello World! \n \tI'm Marchilyn Abunda \n I'm 10 years old");
         
     }
     
