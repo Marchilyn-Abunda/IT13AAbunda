789974c2026978767d169cb20370b155a9de191c
marchilyn <marchilynabunda@gmail.com>
2025-02-10 10:00:14 +0800

    Added hello world java program

---
+++ Abunda_IT13A/src/prelim/hello_world.java
@@ -0,0 +1,13 @@
+
+package prelim;
+
+
+public class hello_world {
+    
+    public static void main(String [] args){
+        
+        System.out.println("Hello World! \n \tI'm Marchilyn Abunda");
+        
+    }
+    
+}
